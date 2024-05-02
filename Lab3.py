a576f6d4ff68971bea7167760932ecd44d14d55e
JunYang <JUNYANG.23@ichat.sp.edu.sg>
2024-05-02 16:58:52 +0800

    Excercise 2

--- Lab3.py
+++ Lab3.py
@@ -11,8 +11,9 @@ def bubble_sort(arr, sorting_order):
 
     # Get number of elements in the list
     n = len(arr_result)
+    skib = list(arr_result)
 
-    if n < 10:
+    if 0 < n < 10:
         # Traverse through all array elements
         for i in range(n - 1):
             # range(n) also work but outer loop will
@@ -33,6 +34,13 @@ def bubble_sort(arr, sorting_order):
                 else:
                     # Return an empty array
                     arr_result = []
+
+    elif n >10 :
+        arr_result = 1
+    elif n == 0:
+        arr_result = 0
+    elif list(arr_result)
+        arr_result == 2
     else:
         arr_result = -1
 
--- Test_Lab3.py
+++ Test_Lab3.py
@@ -1,4 +1,4 @@
-import Lab3
+import Lab3 as test
 
 print("Test_Lab3")
 
@@ -8,7 +8,7 @@ def test_bubble_sort_ascending():
     input_arr = [64, 34, 25, 12, 22, 11, 90]
     test_arr = [11, 12, 22, 25, 34, 64, 90]
 
-    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+    result = test.bubble_sort(input_arr, test.SORT_ASCENDING)
 
     assert (result == test_arr)
 
@@ -17,7 +17,7 @@ def test_bubble_sort_descending():
     input_arr = [64, 34, 25, 12, 22, 11, 90]
     test_arr = [90, 64, 34, 25, 22, 12, 11]
 
-    result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
+    result = test.bubble_sort(input_arr, test.SORT_DESCENDING)
 
     assert (result == test_arr)
 
@@ -25,7 +25,24 @@ def test_bubble_sort_invalid():
     result = []
     input_arr = [64, 34, 25, 12, 22, 11, 90]
 
-    result = Lab3.bubble_sort(input_arr, 3)
+    result = test.bubble_sort(input_arr, 3)
 
     assert (result == [])
-   
+
+def test_bubble_more_ten():
+    result = []
+    input_arr = [20,10,2,24,5,1,8,3,34,11,14.36]
+    result = test.bubble_sort(input_arr,3)
+    assert (result == 1)
+
+def test_bubble_zero():
+    
+    input_arr = []
+    result = test.bubble_sort(input_arr,4)
+    assert (result == 0 )
+
+def test_bubble_not_integer():
+    result = []
+    input_arr = [1.0,2,3,4,4,4,2.3]
+    result = test.bubble_sort(input_arr,5)
+    assert (result == 2 )
